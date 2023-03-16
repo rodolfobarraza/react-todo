023a0b6cbb19aa03187b6e23c166361438591531
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-16 09:48:21 -0600

    89 - Sección computed

--- src/App.jsx
+++ src/App.jsx
@@ -16,26 +16,29 @@ const App = () => {
       </header>
 
       <main className="container mx-auto px-4 mt-8">
-        <div className="bg-white rounded-md inline-block w-full">
-          <article className="flex gap-4 py-4 border-b-gray-400 border-b px-4">
+        <div className="bg-white rounded-md [&>article]:p-4">
+
+          <article className="flex gap-4 border-b-gray-400 border-b">
             <button className="inline-block h-5 w-5 rounded-full border-2 flex-none"></button>
             <p className="grow text-gray-600">Completar curso JavaScript  en línea</p>
             <button className=" flex-none"><CrossIcon/></button>
           </article>
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Completar curso JavaScript  en línea</p>
-            <button><CrossIcon/></button>
+          <article className="flex gap-4 border-b-gray-400 border-b">
+            <button className="inline-block h-5 w-5 rounded-full border-2 flex-none"></button>
+            <p className="grow text-gray-600">Completar curso JavaScript  en línea</p>
+            <button className=" flex-none"><CrossIcon/></button>
           </article>
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Completar curso JavaScript  en línea</p>
-            <button><CrossIcon/></button>
+          <article className="flex gap-4 border-b-gray-400 border-b">
+            <button className="inline-block h-5 w-5 rounded-full border-2 flex-none"></button>
+            <p className="grow text-gray-600">Completar curso JavaScript  en línea</p>
+            <button className=" flex-none"><CrossIcon/></button>
           </article>
-          <section>
-            <span>5 item left</span>
-            <button>Clear completed</button>
+
+          <section className="flex justify-between py-4 px-4">
+            <span className="text-gray-400">5 item left</span>
+            <button className="text-gray-400">Clear completed</button>
           </section>
+          
         </div>
       </main>
 
