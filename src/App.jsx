be92f2e59b1b93f30f24720e2f0c490f3a04189e
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-16 08:20:20 -0600

    clases

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
 const App = () => {
   return (
-    <>
-      <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-cover bg-center">
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
 
-        <header className="container mx-auto px-4">
-          <h1 className="uppercase">Tareas</h1>
+      <header className="container mx-auto px-4 pt-8">
+        <div className="flex justify-between">
+          <h1 className="uppercase text-white font-semibold text-3xl tracking-[0.3em]">Tareas</h1>
           <button>luna</button>
-          <form>
-            <input type="text" placeholder="Nombre Nueva Tarea" />
-          </form>
-        </header>
+        </div>
+        <form>
+          <input type="text" placeholder="Nombre Nueva Tarea" />
+        </form>
+      </header>
 
-        <main className="container mx-auto px-4">
-          <article>
-            <button>circulo</button>
-            <p>Completar curso JavaScript  en línea</p>
-            <button>times</button>
-          </article>
-          <article>
-            <button>circulo</button>
-            <p>Completar curso JavaScript  en línea</p>
-            <button>times</button>
-          </article>
-          <article>
-            <button>circulo</button>
-            <p>Completar curso JavaScript  en línea</p>
-            <button>times</button>
-          </article>
-          <section>
-            <span>5 item left</span>
-            <button>Clear completed</button>
-          </section>
-        </main>
-
+      <main className="container mx-auto px-4">
+        <article>
+          <button>circulo</button>
+          <p>Completar curso JavaScript  en línea</p>
+          <button>times</button>
+        </article>
+        <article>
+          <button>circulo</button>
+          <p>Completar curso JavaScript  en línea</p>
+          <button>times</button>
+        </article>
+        <article>
+          <button>circulo</button>
+          <p>Completar curso JavaScript  en línea</p>
+          <button>times</button>
+        </article>
         <section>
-          <button className="container mx-auto px-4">All</button>
-          <button>Active</button>
-          <button>Completed</button>
+          <span>5 item left</span>
+          <button>Clear completed</button>
         </section>
+      </main>
 
-        <section></section>
+      <section>
+        <button className="container mx-auto px-4">All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </section>
 
-        <footer className="container mx-auto px-4"></footer>
-      </div>
-    </>
+      <section>Drag and drop to reorder list</section>
+
+      <footer className="container mx-auto px-4"></footer>
+    </div>
   )
 }
 
