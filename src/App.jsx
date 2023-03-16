c05dcf7b7b7cc5b78c704999a4f8b38774dd6a69
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-16 09:39:19 -0600

    88 - Artículo + Flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ const App = () => {
       </header>
 
       <main className="container mx-auto px-4 mt-8">
-        <div className="bg-white rounded-md inline-block w-full px-4">
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Completar curso JavaScript  en línea</p>
-            <button><CrossIcon/></button>
+        <div className="bg-white rounded-md inline-block w-full">
+          <article className="flex gap-4 py-4 border-b-gray-400 border-b px-4">
+            <button className="inline-block h-5 w-5 rounded-full border-2 flex-none"></button>
+            <p className="grow text-gray-600">Completar curso JavaScript  en línea</p>
+            <button className=" flex-none"><CrossIcon/></button>
           </article>
           <article>
             <button className="inline-block h-5 w-5 rounded-full border-2"></button>
