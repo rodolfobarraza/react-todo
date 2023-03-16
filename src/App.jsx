e2679cd5790fcd31f0c9f50bf87734c35e1c5e56
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-16 08:19:54 -0600

    Estructura

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,46 @@ const App = () => {
   return (
     <>
       <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-cover bg-center">
-        <h1 className="uppercase">Tareas</h1>
-        <button>luna</button>
+
+        <header className="container mx-auto px-4">
+          <h1 className="uppercase">Tareas</h1>
+          <button>luna</button>
+          <form>
+            <input type="text" placeholder="Nombre Nueva Tarea" />
+          </form>
+        </header>
+
+        <main className="container mx-auto px-4">
+          <article>
+            <button>circulo</button>
+            <p>Completar curso JavaScript  en línea</p>
+            <button>times</button>
+          </article>
+          <article>
+            <button>circulo</button>
+            <p>Completar curso JavaScript  en línea</p>
+            <button>times</button>
+          </article>
+          <article>
+            <button>circulo</button>
+            <p>Completar curso JavaScript  en línea</p>
+            <button>times</button>
+          </article>
+          <section>
+            <span>5 item left</span>
+            <button>Clear completed</button>
+          </section>
+        </main>
+
+        <section>
+          <button className="container mx-auto px-4">All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </section>
+
+        <section></section>
+
+        <footer className="container mx-auto px-4"></footer>
       </div>
     </>
   )
