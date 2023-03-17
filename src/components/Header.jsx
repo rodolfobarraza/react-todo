f0561be9831a336e6554113e3beb45ea3f9ffb65
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-17 09:03:42 -0600

    111 - DarkMode en SO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,19 @@ import { useState, useEffect } from "react";
 import IconMoon from "./icons/IconMoon";
 import IconSun from "./icons/IconSun";
 
+const initialStateDarkMode = localStorage.getItem('theme') === 'dark';
+
 const Header = () => {
 
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(initialStateDarkMode);
 
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark");
+      localStorage.setItem ('theme', 'dark');
     } else {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem ('theme', 'light');
     }
   }, [darkMode]);
 
--- src/utils/darkMode.js
+++ src/utils/darkMode.js
@@ -1,16 +1,7 @@
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-    localStorage.theme = 'dark'
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
 } else {
-    document.documentElement.classList.remove('dark')
-    localStorage.theme = 'light'
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
 }
-
-// Whenever the user explicitly chooses light mode
-//localStorage.theme = 'light'
-
-// Whenever the user explicitly chooses dark mode
-//localStorage.theme = 'dark'
-
-// Whenever the user explicitly chooses to respect the OS preference
-//localStorage.removeItem('theme')
