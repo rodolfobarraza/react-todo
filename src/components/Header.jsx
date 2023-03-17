2481c2be3ae5c817fdf939c885eec1e71c10b511
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-17 08:27:12 -0600

    108 - Dark Mode: Ícono Luna y Sol

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import IconMoon from "./icons/IconMoon";
+import IconSun from "./icons/IconSun";
 
 const Header = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClickToggleTheme = () => {}
+
   return (
     <header className="container mx-auto px-4 pt-8">
       <div className="flex justify-between">
         <h1 className="uppercase text-white font-semibold text-3xl tracking-[0.3em]">Tareas</h1>
-        <button><IconMoon fill="#FFF"/></button>
+        <button onClick={() => setDarkMode(!darkMode)}>
+          {
+            darkMode ? <IconSun fill="#ffa41d"/> : <IconMoon fill="#FFF"/>
+          }
+        </button>
       </div>
     </header>
   );
